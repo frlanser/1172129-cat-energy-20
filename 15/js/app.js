a60197a8f5c61731b6821ffeac8fc0476a7bfa38
Keks <keks@htmlacademy.ru>
2020-09-07 13:08:30 +0300

    :heavy_check_mark: Сборка #15

--- 15/js/app.js
+++ 15/js/app.js
@@ -1,1 +1,1 @@
-$(function(){$("#nav_toggle").on("click",function(t){t.preventDefault(),$(this).toggleClass("active"),$("#nav").toggleClass("active")})});
+$(function(){$("#nav").removeClass("active"),$("#nav_toggle").on("click",function(e){e.preventDefault(),$(this).toggleClass("active"),$("#nav").toggleClass("active")})});
